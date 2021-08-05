78aa7ae777d795e19b1266fb520e10545b6fb830
Pasindu-Lakmal <73387775+Pasindu-Lakmal@users.noreply.github.com>
2021-08-05 08:05:04 +0530

    Update q5.scala

--- Assinment 2/q5.scala
+++ Assinment 2/q5.scala
@@ -1,19 +1,20 @@
-import scala.io.StdIn.readInt;
+package Exersice
 
 object q5 extends App {
-    var b:Int = 0;
-    def sum(a:Int): Unit= {
-        if(a > 0) {
-        if(a%2 == 0) {
-            b = b + a;
-        } 
-        sum(a - 1);
-        }
-        else print(b);
-    }
 
-    print("Input a value:");
-    var value = readInt();
-    sum(value-1);
-    
+  def isEven(n:Int):Boolean=n match{
+    case 0 => true
+    case _ => isOdd(n-1)
+  }
+
+  def isOdd(n:Int):Boolean= !(isEven(n))
+
+  def evenSum(n:Int,i:Int=0,sum:Int=0):Int=i match{
+    case x if (x==n) => sum
+    case x if ((x<n) && (isOdd(i))) => evenSum(n,i+1,sum)
+    case x if ((x<n) && (isEven(i))) => evenSum(n,i+1,sum+i)
+  }
+
+  println(evenSum(5))
+
 }
