7f56ef5dc92b8f4c61b76f2d588b9d4ff39f638a
Pasindu-Lakmal <73387775+Pasindu-Lakmal@users.noreply.github.com>
2021-08-05 08:04:10 +0530

    Update q3.scala

--- Assinment 2/q3.scala
+++ Assinment 2/q3.scala
@@ -1,14 +1,8 @@
-import scala.io.StdIn.readInt;
+package Exersice
 
 object q3 extends App {
-    var b:Int = 0;
-    def sum(a:Int): Unit={
-        b = b + a; 
-        if(a>0) sum(a - 1);
-        else print(b); 
-    }
-
-    print("Input a value:");
-    var value = readInt();
-    sum(value);
+  def sum(n:Int):Int=
+    if(n==0)  0
+    else n+sum(n-1)
+  println(sum(5))
 }
