8af0c970cf4115a45b6e906173a3079b9231e5ab
Pasindu-Lakmal <73387775+Pasindu-Lakmal@users.noreply.github.com>
2021-08-05 08:02:58 +0530

    Update q2.scala

--- Assinment 2/q2.scala
+++ Assinment 2/q2.scala
@@ -1,24 +1,22 @@
-import scala.io.StdIn.readInt;
+package Exersice
 
-object q2 extends App {
-    def gcd(a:Int, b:Int):Int=b match{
-        case 0 => a;
-        case x if x>a => gcd(x,a);
-        case x => gcd(x,a%x);
-    }
-    def prime(a:Int, b:Int = 2): Boolean = b match{
-        case x if(x==a)=>true;
-        case x if gcd(a,x)>1 => false;
-        case x => prime(a,x+1);
-    }
-    def primeSeq(n:Int) : Unit={
-        if(n>1){
-            primeSeq(n-1);
-            if (prime(n)) println(n);
-        }
-    }
+object q2 extends App   {
+  def gcd(x:Int, y:Int):Int=y match {
+    case 0 => x
+    case y if (y>x)=>gcd(y,x)
+    case _ =>gcd(y,x%y)
+  }
 
-    print("Input a value:");
-    var value = readInt();
-    primeSeq(value);
+  def prime(p:Int, n:Int=2):Boolean=n match {
+    case x if(p==x)=>true
+    case x if (gcd(p,x)>1)=>false
+    case x=>prime(p,x+1)
+  }
+
+  def primeSeq(a:Int):Unit={
+    if(a>2)primeSeq(a-1)
+    if(prime(a))println(a)
+
+  }
+  primeSeq(10)
 }
