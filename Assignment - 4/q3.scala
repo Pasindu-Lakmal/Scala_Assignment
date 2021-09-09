0aaea6217a32b4032294f664d18111d172885f08
Pasindu-Lakmal <73387775+Pasindu-Lakmal@users.noreply.github.com>
2021-09-09 09:05:20 +0530

    Update q3.scala

--- Assignment - 4/q3.scala
+++ Assignment - 4/q3.scala
@@ -1,46 +1,27 @@
-object q3 extends App {
-    var accountList:List[Account] = List()
+package q3
+object q3 extends App{
+  val a = new Account("980930416v", 15123565,5000.00)
+  val b = new Account("556641282v", 21548468, 4350.50)
+  a.transfer(b,550.00)
+  println(a)
+  println(b)
+  class Account(id:String, n:Int, bal:Double){
+    var nic:String = id
+    var accno:Int = n
+    var balance:Double = bal
 
-    def accCreate(nic:String, accId: Int):Unit = {
-        val acc = new Account(nic, accId)
-        accountList = accountList ::: acc :: Nil
-        println(accountList)
+    def withdraw(amount:Double)={
+      this.balance = this.balance - amount
     }
 
-    val find = (a:Int, b:List[Account]) => b.filter(account => account.accId.equals(a))
-
-    /*              Driver Code                */
-    
-    accCreate("1",1)
-    accCreate("2",2)
-
-    //deposit money
-    find(1, accountList)(0).deposit(1000)
-    println(find(1, accountList)(0))
-
-    //transfer money
-    find(1, accountList)(0).transfer(2, 100.0)
-    println(find(2, accountList)(0))
-}
-
-class Account(nic:String, val accId: Int, var balance: Double = 0.0){
-
-    def withdrow(amount:Double) : Unit = {
-        this.balance = this.balance - amount
+    def deposit(amount:Double)={
+      this.balance = this.balance + amount
     }
-
-    def deposit(amount:Double) : Unit = {
-        this.balance = this.balance + amount
+    def transfer(a:Account,b:Double)={
+      this.withdraw(b)
+      a.deposit(b)
     }
+    override def toString = s"[${nic} : ${accno} : ${balance}]"
 
-    def transfer(account:Int, amount:Double) : Unit = {
-        val transferAcc = Q4.find(account, Q4.accountList)
-        if (balance < 0.0) println("Insufficient balance")
-        else {
-            this.withdrow(amount)
-            transferAcc(0).deposit(amount)
-        }
-    }
-
-    override def toString = "["+nic+":"+accId +":"+ balance+"]"
+  }
 }
